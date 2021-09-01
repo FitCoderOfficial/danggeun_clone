9103a46f7568ac4663b83c3a042c882869c61f84
Shin-Sae-byeok <toqur1219@naver.com>
2021-09-01 18:42:15 +0900

    feat:list view design

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -27,63 +27,63 @@ class _HomeState extends State<Home> {
         "title": "LA갈비 5kg팔아요",
         "location": "제주 제주시 아라동",
         "price": "100000",
-        "likes": "5",
+        "likes": "82",
       },
       {
         "image": "assets/images/ara-3.jpg",
         "title": "치약팝니다",
         "location": "제주 제주시 아라동",
         "price": "5000",
-        "likes": "0",
+        "likes": "72",
       },
       {
         "image": "assets/images/ara-4.jpg",
         "title": "LA갈비 5kg팔아요",
         "location": "제주 제주시 아라동",
         "price": "100000",
-        "likes": "5",
+        "likes": "155",
       },
       {
         "image": "assets/images/ara-5.jpg",
         "title": "치약팝니다",
         "location": "제주 제주시 아라동",
         "price": "5000",
-        "likes": "0",
+        "likes": "84",
       },
       {
         "image": "assets/images/ara-6.jpg",
-        "title": "LA갈비 5kg팔아요",
+        "title": "갤럭시 64g",
         "location": "제주 제주시 아라동",
         "price": "100000",
-        "likes": "5",
+        "likes": "125",
       },
       {
         "image": "assets/images/ara-7.jpg",
         "title": "치약팝니다",
         "location": "제주 제주시 아라동",
         "price": "5000",
-        "likes": "0",
+        "likes": "99",
       },
       {
         "image": "assets/images/ara-8.jpg",
         "title": "LA갈비 5kg팔아요",
         "location": "제주 제주시 아라동",
         "price": "100000",
-        "likes": "5",
+        "likes": "63",
       },
       {
         "image": "assets/images/ara-9.jpg",
         "title": "치약팝니다",
         "location": "제주 제주시 아라동",
         "price": "5000",
-        "likes": "0",
+        "likes": "32",
       },
       {
         "image": "assets/images/ara-10.jpg",
         "title": "치약팝니다",
         "location": "제주 제주시 아라동",
         "price": "5000",
-        "likes": "0",
+        "likes": "11",
       },
     ];
   }
@@ -118,16 +118,56 @@ class _HomeState extends State<Home> {
 
   Widget _bodyWidget() {
     return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       itemBuilder: (BuildContext _context, int index) {
-        return Container(
-            child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              child: Image.asset(datas[index]["image"] ?? ''),
-            ),
-          ],
-        ));
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Container(
+              child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: Image.asset(
+                  datas[index]["image"] ?? '',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              // SizedBox(width: 20),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  padding: const EdgeInsets.only(left: 20),
+                  // color: Colors.blue,
+                  child: Column(
+                    // mainAxisAlignment: ,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(datas[index]["title"] ?? ''),
+                      Text(datas[index]["location"] ?? ''),
+                      Text(datas[index]["price"] ?? ''),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/heart_off.svg",
+                              width: 13,
+                            ),
+                            SizedBox(width: 5),
+                            Text(datas[index]["likes"] ?? ''),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+        );
       },
       itemCount: 10,
       separatorBuilder: (BuildContext _context, int index) {
